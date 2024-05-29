ed9eb9794746221027f2f08a724758d9bb8b4d14
matiasbarriosled <matiasbarriosled@gmail.com>
2024-05-28 23:59:21 -0300

    actulaizacion

---
+++ dashboard/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Proyecto integrador numero 2:")
+st.subheader("Siniestros viales en CABA")
+st.header("pruebas de streamlit")
+st.text("aqui agregare los textos para poder detallar los conceptos e ideas de cada grafico... creo")
+st.markdown('---')
